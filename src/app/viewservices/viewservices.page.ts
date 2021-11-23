7edf7a6656e6a7e57cff19de6301a1c472db6844
Manuel Amores González <amoresgonzalezmanuel@gmail.com>
2021-11-22 21:45:42 -0600

    Evaluar signo de reservar

--- src/app/viewservices/viewservices.page.ts
+++ src/app/viewservices/viewservices.page.ts
@@ -9,19 +9,23 @@ import { conexionService } from '../Services/conexion.service';
 export class ViewservicesPage implements OnInit {
 
   nombrefiltrado = '';
+  valor= false;
 
   public prueba = [
     {
       Operador: "TRANSPORTES DEL ATLANTICO CARIBEÑO SOCIEDAD ANONIMA  ",
       PorcentajePavimentado: "0,95441",
+      reservado: true
     },
     {
       Operador: "BUSES SAN MIGUEL HIGUITO SOCIEDAD ANONIMA  ",
       PorcentajePavimentado: "0,695845",
+      reservado: true
     },
     {
       Operador: "COOPERATIVA DE AUTOBUSEROS NACIONALES ASOCIADOS R L  ",
       PorcentajePavimentado: "0,379054",
+      reservado: false
     }    
   ]
   
